5d4d42e99a7d4e940d1cdf7c619a035916a1cbae
Issa Jubril <issa.jubril@andela.com>
2015-11-20 16:41:32 +0100

    [Chore] added a staticfile folder

--- djangobucketlist/settings/common.py
+++ djangobucketlist/settings/common.py
@@ -118,7 +118,7 @@ STATICFILES_DIRS = (
 STATIC_URL = '/static/'
 
 # STATIC_ROOT = 'staticfiles'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 REST_FRAMEWORK = {
     # 'PAGE_SIZE': 10,
