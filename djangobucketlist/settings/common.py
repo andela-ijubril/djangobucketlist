429be5162fae7db1c329cfc5a81658ab87621449
Issa Jubril <issa.jubril@andela.com>
2015-11-20 19:06:49 +0100

    [fix] make modifications to static files

--- djangobucketlist/settings/common.py
+++ djangobucketlist/settings/common.py
@@ -112,13 +112,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '../static'),
+    os.path.join(BASE_DIR, '..', 'static'),
 )
 
 STATIC_URL = '/static/'
 
 # STATIC_ROOT = 'staticfiles'
-STATIC_ROOT = os.path.join(BASE_DIR, '../staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, '..', 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
