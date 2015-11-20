796e3e00510110f214fcf3398b909f095d6cae7c
Issa Jubril <issa.jubril@andela.com>
2015-11-20 18:14:25 +0100

    [Chore] updated the requirements file

--- djangobucketlist/wsgi.py
+++ djangobucketlist/wsgi.py
@@ -16,6 +16,5 @@ from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangobucketlist.settings")
 
-
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
