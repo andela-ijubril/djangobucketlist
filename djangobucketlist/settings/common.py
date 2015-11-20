c16597826c8d5f430bde7effc93d71fbeaa06755
Issa Jubril <issa.jubril@andela.com>
2015-11-20 18:50:31 +0100

    [fix] modify static files

--- djangobucketlist/settings/common.py
+++ djangobucketlist/settings/common.py
@@ -115,7 +115,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, '../static'),
 )
 
-STATIC_URL = '../static/'
+STATIC_URL = '/static/'
 
 # STATIC_ROOT = 'staticfiles'
 STATIC_ROOT = os.path.join(BASE_DIR, '../staticfiles')
