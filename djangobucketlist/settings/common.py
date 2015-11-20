577411793c1c4f7e945ea7f1b3bd45e3c137e075
Issa Jubril <issa.jubril@andela.com>
2015-11-20 18:33:50 +0100

    [Fix] fix static files issues

--- djangobucketlist/settings/common.py
+++ djangobucketlist/settings/common.py
@@ -118,7 +118,7 @@ STATICFILES_DIRS = (
 STATIC_URL = '/static/'
 
 # STATIC_ROOT = 'staticfiles'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, '../staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
