f1ddcf556068f7664b8417506de0d08c822aadb6
Issa Jubril <issa.jubril@andela.com>
2015-12-14 22:35:50 +0100

    [Fixes] Codebase refactor

--- bucketlistapi/urls.py
+++ bucketlistapi/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     url(r'^login/$', views.TokenView.as_view(), name='login_user'),
     url(r'^users/$', views.UserList.as_view(), name='all_users'),
     url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
-    url(r'^bucketlists/$', views.BucketlistView.as_view(), name='bucket_list'),
+    url(r'^bucketlists/$', views.BucketlistView.as_view(), name='bucketlist'),
     url(r'^bucketlists/(?P<bucket_id>[0-9]+)/$', views.BucketListDetailView.as_view(), name='bucketlist_detail'),
     url(r'^bucketlists/(?P<bucket_id>[0-9]+)/items/$', views.BucketlistItemView.as_view(), name='bucketlist_item'),
     url(r'^bucketlists/(?P<bucket_id>[0-9]+)/items/(?P<item_id>[0-9]+)$', views.BucketlistItemDetailView.as_view(), name='item_detail'),
--- bucketlistapp/urls.py
+++ bucketlistapp/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     url(r'^bucketlists/(?P<bucket_id>[0-9]+)/$', views.UpdateBucketlistView.as_view(), name='update_bucket_list'),
     url(r'^bucketlists/(?P<bucketlist>[0-9]+)/items/$', views.BucketlistItemAppView.as_view(), name='bucketlist_item'),
     url(r'^bucketlists/(?P<bucketlist>[0-9]+)/items/(?P<item>[0-9]+)/$', views.UpdateBucketlistItemView.as_view(), name='update_bucketlist_item'),
-    url(r'^bucketlists/(?P<bucketlist>[0-9]+)/items/(?P<item>[0-9]+)/status/$', views.ItemStatusView.as_view(), name='bucket_list_item_status'),
+    url(r'^bucketlists/(?P<bucketlist>[0-9]+)/items/(?P<item>[0-9]+)/status/$', views.ItemStatusView.as_view(), name='bucketlist_item_status'),
 ]
